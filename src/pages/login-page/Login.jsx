c52ba870f98ebc8a50e1184aee97ecbf8f50e9a8
Anna Shatilova <anxet@mail.ru>
2023-11-17 17:22:16 +0300

    Страница логина - верстка, стили. Создан общий компонент для фиолетовой кнопки в App.styles. Этот компонент добавлен на страницы info-page, profile-page

--- src/pages/login-page/Login.jsx
+++ src/pages/login-page/Login.jsx
@@ -1,3 +1,34 @@
+import { Link } from 'react-router-dom'
+import * as S from './LoginAndRegistration.styles'
+
 export const Login = () => {
-  return <h1>страница авторизации</h1>
+  return (
+    <S.PageContainer>
+      <S.ModalForm>
+        <Link to="/login">
+          <S.ModalLogo>
+            <S.ModalLogoImage src="/img/logo_modal.png" alt="logo" />
+          </S.ModalLogo>
+        </Link>
+        <S.Inputs>
+          <S.ModalInput
+            type="text"
+            name="login"
+            placeholder="Логин"
+          />
+          <S.ModalInput
+            type="password"
+            name="password"
+            placeholder="Пароль"
+          />
+        </S.Inputs>
+        <S.Buttons>
+          <S.PrimaryButton>Войти</S.PrimaryButton>
+          <Link to="/register">
+            <S.SecondaryButton>Зарегистрироваться</S.SecondaryButton>
+          </Link>
+        </S.Buttons>
+      </S.ModalForm>
+    </S.PageContainer>
+  )
 }
