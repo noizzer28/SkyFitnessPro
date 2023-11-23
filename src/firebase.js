342a1d8d3c3ce20f333a198c17aadfd8a187a75d
ArseniyKhal <asmn@mail.ru>
2023-11-23 18:03:08 +0500

    test

--- src/firebase.js
+++ src/firebase.js
@@ -7,6 +7,13 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENFER_ID,
   appId: process.env.REACT_APP_FIREBASE_API_ID,
+
+  //   apiKey: 'AIzaSyB-wRQxSLi02iluYOtQb3MIYHHKzhUfkMA',
+  //   authDomain: 'fitness-pro-ae1f4.firebaseapp.com',
+  //   projectId: 'fitness-pro-ae1f4',
+  //   storageBucket: 'fitness-pro-ae1f4.appspot.com',
+  //   messagingSenderId: '1034773898251',
+  //   appId: '1:1034773898251:web:7dfa7ec88e44363287d30e',
 }
 
 const app = initializeApp(firebaseConfig)
