cafa7b1a0368bacde5ab97d71958d5327dbd5ef9
ArseniyKhal <asmn@mail.ru>
2023-11-24 14:14:08 +0500

    bg

--- src/api.js
+++ src/api.js
@@ -6,15 +6,13 @@ import {
 
 const auth = getAuth()
 // авторизация пользователя
-
 export async function login({ email, pass }) {
-  console.log('login')
   const authData = await signInWithEmailAndPassword(auth, email, pass)
   return authData.user
 }
 
+// регистрация пользователя
 export async function registration({ email, pass }) {
-  console.log('registration')
   const regData = await createUserWithEmailAndPassword(auth, email, pass)
   return regData.user
 }
