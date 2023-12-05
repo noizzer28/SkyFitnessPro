3530d3e40a3a13b7d47b0a7fd07432520f321cce
ArseniyKhal <asmn@mail.ru>
2023-12-05 15:17:42 +0500

    test 8

--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -8,6 +8,9 @@ import * as S from './MainPage.styles'
 export const Main = ({ error }) => {
   const { courses } = useSelector((state) => state.courses)
   console.log(courses)
+  setTimeout(() => {
+    console.log(courses)
+  }, 2500)
   // формируем список курсов
   const mapCoursesList = courses?.map((courseCard) => (
     <Card
