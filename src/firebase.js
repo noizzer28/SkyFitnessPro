0aeea2117501b056cf7b9387a86d97581dc9d3de
ArseniyKhal <asmn@mail.ru>
2024-02-05 12:12:49 +0500

    readme

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app'
 const firebaseConfig = {
   apiKey: 'AIzaSyB-wRQxSLi02iluYOtQb3MIYHHKzhUfkMA',
   authDomain: 'fitness-pro-ae1f4.firebaseapp.com',
-  databaseURL:
-    'https://fitness-pro-ae1f4-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: 'fitness-pro-ae1f4',
   storageBucket: 'fitness-pro-ae1f4.appspot.com',
   messagingSenderId: '1034773898251',
   appId: '1:1034773898251:web:7dfa7ec88e44363287d30e',
+  databaseURL:
+    'https://fitness-pro-ae1f4-default-rtdb.europe-west1.firebasedatabase.app/',
 }
 // const firebaseConfig = {
 //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
